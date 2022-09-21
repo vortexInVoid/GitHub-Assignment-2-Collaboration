f62a4b8d8fd6a8b6c6b54d192de1cc1a0203fad4
seldeniz-cs <91663119+seldeniz-cs@users.noreply.github.com>
2022-09-21 06:43:29 +0300

    Add files via upload
    Sum of even and odd numbers part updated.

--- lab0_2.java
+++ lab0_2.java
@@ -1,48 +1,48 @@
 import java.util.Scanner;
 public class lab0_2 {
-
     public static void main(String[] args) {
-
-
-
         Scanner scan = new Scanner(System.in);
-
         int n;
         System.out.print("Enter the size of the array:");
         n = scan.nextInt();
-
         int numbers[] = generateRandomArray( n );
-
         displayer(numbers);
-
-
     }
     public static int[] generateRandomArray( int lengthOfArray )
     {
 	int[] randomArray = new int[lengthOfArray];
+	double randomValue;
 	for ( int i = 0; i < lengthOfArray - 1; i++ )
 	{
 		randomArray[i] = (int) ( 101 * Math.random( ) );
 	}
-
 	return randomArray;
     }	
 	
     public static void displayer(int numbers[]){
+        int sumofOdd = 0;
+        int sumofEven = 0;
         System.out.print("Your array is this: ");
 		for (int i=0;i<numbers.length;i++){
 			System.out.print( (i+1) +  numbers[i] + " ");
 		}
-        System.out.println("Sum of the even indexes is : ");
+        
+        System.out.print("Sum of the even indexes is : ");
+        
         for(int i=0;i<numbers.length;i=i+2){
-            System.out.println((i+1) + numbers[i] + "+" );
+            sumofEven = sumofEven + ((i+1) + numbers[i]);
         }
-        System.out.println("Sum of the odd indexes is : ");
+        System.out.println(sumofEven);
+        
+        System.out.print("Sum of the odd indexes is : ");
+        
         for(int i=1;i<numbers.length;i=i+2){
-            System.out.println((i+1) + numbers[i] + "+" );
-        }
-	}
+            sumofOdd = sumofOdd + ((i+1) + numbers[i]);
 
+        }
+        System.out.println(sumofOdd);
+	
+    }
     
         public int[] averageMinus(int arr[])
         {
@@ -61,4 +61,4 @@ public class lab0_2 {
     
     }
     
-}
+
